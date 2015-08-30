e63c13bcb1264e35af1d260810af390ebf4fc0d9
Brucechen13 <chenchi_94@163.com>
2015-08-30 22:09:05 +0800

    add fragment

--- app/src/main/java/com/chen/soft/MainActivity.java
+++ app/src/main/java/com/chen/soft/MainActivity.java
@@ -1,38 +1,127 @@
 package com.chen.soft;
 
 import android.app.Activity;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.support.v4.app.FragmentActivity;
+import android.widget.TextView;
+
+import com.chen.soft.fragment.FragmentCallback;
+import com.chen.soft.fragment.FragmentUser;
+import com.chen.soft.fragment.FragmentUtils;
 
 
-public class MainActivity extends Activity {
+public class MainActivity extends FragmentActivity implements
+        TabView.OnTabChangeListener, FragmentCallback {
+
+    private Fragment mCurrentFragment;
+    private FragmentManager fragmentManager;
+
+    private TabView mTabView;
+    private TextView mTitleTextView;
+
+    /**
+     * 上一次的状态
+     */
+    private int mPreviousTabIndex = 1;
+    /**
+     * 当前状态
+     */
+    private int mCurrentTabIndex = 1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        fragmentManager = getSupportFragmentManager();
+        mCurrentTabIndex = 0;
+        mPreviousTabIndex = 0;
+        setupViews();
+    }
+
+    private void setupViews() {
+        // TODO Auto-generated method stub
+        mTitleTextView = (TextView) findViewById(R.id.text_title);
+        mTitleTextView.setText(R.string.text_tab_message);
+        mTabView = (TabView) findViewById(R.id.view_tab);
+        mTabView.setOnTabChangeListener(this);
+        mTabView.setCurrentTab(mCurrentTabIndex);
+        mCurrentFragment = new FragmentUser();
+        FragmentUtils.replaceFragment(fragmentManager, R.id.layout_content,
+                FragmentUser.class, null, false);
+
+    }
+
+    private void replaceFragment(Class<? extends Fragment> newFragment) {
+
+        mCurrentFragment = FragmentUtils
+                .switchFragment(fragmentManager, R.id.layout_content,
+                        mCurrentFragment, newFragment, null, false);
+    }
+
+    private void replaceFragment(Class<? extends Fragment> newFragment,
+                                 Bundle bundle) {
+
+        mCurrentFragment = FragmentUtils.switchFragment(fragmentManager,
+                R.id.layout_content, mCurrentFragment, newFragment, bundle,
+                false);
     }
 
     @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
+    public void onFragmentCallback(Fragment fragment, int id, Bundle args) {
+        // TODO Auto-generated method stub
+        mTabView.setCurrentTab(1);
     }
 
     @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
+    public void onTabChange(String tag) {
+        // TODO Auto-generated method stub
 
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
+        if (tag != null) {
+//            if (tag.equals("message")) {
+//                mPreviousTabIndex = mCurrentTabIndex;
+//                mCurrentTabIndex = 0;
+//                mTitleTextView.setText(R.string.text_tab_message);
+//                replaceFragment(FragmentMap.class);
+//                // 检查，如果没有登录则跳转到登录界面
+//				/*
+//				 * final UserConfigManager manager =
+//				 * UserConfigManager.getInstance(); if (manager.getId() <= 0) {
+//				 * startActivityForResult(new Intent(this, LoginActivity.class),
+//				 * BaseActivity.REQUEST_OK_LOGIN); }
+//				 */
+//            } else if ("service".equals(tag)) {
+//                mPreviousTabIndex = mCurrentTabIndex;
+//                mCurrentTabIndex = 1;
+//                mTitleTextView.setText(R.string.text_tab_service);
+//                replaceFragment(FragmentSearch.class);
+//            } else if (tag.equals("personal")) {
+//                mPreviousTabIndex = mCurrentTabIndex;
+//                mCurrentTabIndex = 2;
+//                mTitleTextView.setText(R.string.text_tab_profile);
+//                replaceFragment(FragmentUser.class, bundle);
+//                // 检查，如果没有登录则跳转到登录界面
+//				/*
+//				 * final UserConfigManager manager =
+//				 * UserConfigManager.getInstance(); if (manager.getId() <= 0) {
+//				 * startActivityForResult(new Intent(this, LoginActivity.class),
+//				 * BaseActivity.REQUEST_OK_LOGIN); }
+//				 */
+//            } else if (tag.equals("settings")) {
+//                mPreviousTabIndex = mCurrentTabIndex;
+//                mCurrentTabIndex = 3;
+//                mTitleTextView.setText(R.string.text_tab_setting);
+//                replaceFragment(FragmentSetting.class);
+//                // 检查，如果没有登录则跳转到登录界面
+//				/*
+//				 * final UserConfigManager manager =
+//				 * UserConfigManager.getInstance(); if (manager.getId() <= 0) {
+//				 * startActivityForResult(new Intent(this, LoginActivity.class),
+//				 * BaseActivity.REQUEST_OK_LOGIN); }
+//				 */
+//            }
         }
 
-        return super.onOptionsItemSelected(item);
     }
 }
---
+++ app/src/main/java/com/chen/soft/fragment/FragmentUser.java
@@ -0,0 +1,31 @@
+package com.chen.soft.fragment;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.MotionEvent;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.chen.soft.R;
+
+/**
+ * Created by chenchi_94 on 2015/8/30.
+ */
+public class FragmentUser extends BaseFragment implements View.OnClickListener {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_user, container, false);
+    }
+
+    @Override
+    public void onClick(View v) {
+
+    }
+
+    @Override
+    public boolean onTouch(View v, MotionEvent event) {
+        return false;
+    }
+}
